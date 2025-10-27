af636ec4ef63911aeeb2ad2ce5fe0d510ef175b0
adrian161103 <adriankeomaa@gmail.com>
2025-10-27 17:40:28 -0300

    añadido reactrouter para facilitar las rutas

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";
+import { BrowserRouter } from "react-router-dom";
+import AppRoutes from "./routes";
 import "./index.css";
 import { LanguageProvider } from "./context/LanguageContext";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-        <LanguageProvider>
-    <App />
+    <LanguageProvider>
+      <BrowserRouter>
+        <AppRoutes />
+      </BrowserRouter>
     </LanguageProvider>
   </React.StrictMode>
 );
